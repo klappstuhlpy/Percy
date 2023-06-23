ff7e9fba29bfbd31ab0904dcc7e9be67507162e7
Benedikt Schredl <benedikt.schredl@gmx.de>
2023-06-23 23:00:12 +0200

    fixed root folder locating

--- cogs/utils/config.py
+++ cogs/utils/config.py
@@ -58,7 +58,7 @@ class Config(Generic[_T]):
         with open(self.real_path(temp), 'w', encoding='utf-8') as tmp:
             json.dump(self._db.copy(), tmp, ensure_ascii=True, cls=self.encoder, separators=(',', ':'))
 
-        os.replace(temp, self.name)
+        os.replace(self.real_path(temp), self.name)
 
     async def save(self) -> None:
         async with self.lock:
