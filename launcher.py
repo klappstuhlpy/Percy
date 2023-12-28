1bc6af0fddb8dd2ab746d7be74aa40d853e1fc8d
Klappstuhl <benedikt.schredl@gmx.de>
2023-12-28 17:33:42 +0100

    code styling for launcher.py

--- launcher.py
+++ launcher.py
@@ -1,6 +1,5 @@
 import asyncio
 import contextlib
-import datetime
 import json
 import logging
 import os
@@ -10,7 +9,7 @@ import traceback
 import uuid
 from logging.handlers import RotatingFileHandler
 from pathlib import Path
-from typing import TypedDict, Any, TYPE_CHECKING, cast
+from typing import TypedDict, TYPE_CHECKING, cast
 
 import asyncpg
 import click
@@ -23,7 +22,7 @@ from bot import Percy
 from cogs.utils.constants import REVISION_FILE, BOT_BASE_FOLDER
 
 try:
-    import uvloop  # type: ignore
+    import uvloop  # noqa
 except ImportError:
     pass
 else:
@@ -61,15 +60,12 @@ class Migrations:
     def ensure_path(self) -> None:
         self.root.mkdir(exist_ok=True)
 
-    def load_metadata(self) -> dict[str, int | Any] | Any:
+    def load_metadata(self) -> Revisions:
         try:
             with open(self.filename, 'r', encoding='utf-8') as fp:
-                return json.load(fp)
+                return Revisions(**json.load(fp))
         except FileNotFoundError:
-            return {
-                'version': 0,
-                'database_uri': discord.utils.MISSING,
-            }
+            return Revisions(version=0, database_uri='')
 
     def get_revisions(self) -> dict[int, Revision]:
         result: dict[int, Revision] = {}
@@ -81,15 +77,13 @@ class Migrations:
 
         return result
 
-    def dump(self) -> dict[str, int | str]:
-        return {
-            'version': self.version,
-            'database_uri': self.database_uri,
-        }
+    def dump(self) -> Revisions:
+        return Revisions(version=self.version, database_uri=self.database_uri)
 
     def load(self) -> None:
         self.ensure_path()
         data = self.load_metadata()
+
         self.version: int = data['version']
         self.database_uri: str = data['database_uri']
 
@@ -98,9 +92,9 @@ class Migrations:
         with open(temp, 'w', encoding='utf-8') as tmp:
             json.dump(self.dump(), tmp)
 
-        # atomically move the file
         os.replace(temp, self.filename)
 
+    @property
     def is_next_revision_taken(self) -> bool:
         return self.version + 1 in self.revisions
 
@@ -115,7 +109,7 @@ class Migrations:
 
         stub = (
             f'-- Revises: V{self.version}\n'
-            f'-- Creation Date: {datetime.datetime.utcnow()} UTC\n'
+            f'-- Creation Date: {discord.utils.utcnow()} UTC\n'
             f'-- Reason: {reason}\n\n'
         )
 
@@ -240,14 +234,15 @@ def setup_logging():
         get_logger('charset_normalizer').setLevel(logging.ERROR)
 
         root_log.setLevel(logging.INFO)
-        handler = RotatingFileHandler(filename=os.path.join(BOT_BASE_FOLDER, 'percy.log'),
-                                      encoding='utf-8', mode='w', maxBytes=max_bytes, backupCount=5)
+        handler = RotatingFileHandler(
+            filename=os.path.join(BOT_BASE_FOLDER, 'percy.log'),
+            encoding='utf-8', mode='w', maxBytes=max_bytes, backupCount=5
+        )
         handler.setFormatter(fmt)
         root_log.addHandler(handler)
 
         yield
     finally:
-        # __exit__
         handlers = root_log.handlers[:]
         for hdlr in handlers:
             hdlr.close()
@@ -276,23 +271,23 @@ async def create_pool() -> asyncpg.Pool:
         command_timeout=300,
         max_size=20,
         min_size=20,
-    )  # type: ignore
+    )
 
 
 async def run_bot():
     discord.VoiceClient.warn_nacl = False
-    log = get_logger()
+    _log = get_logger()
 
     try:
         pool = await create_pool()
     except Exception:
-        click.echo('Could not set up PostgreSQL. Exiting.', file=sys.stderr)
-        log.exception('Could not set up PostgreSQL. Exiting.')
+        click.echo('Unable to establish a connection with PostgreSQL. Exiting.', file=sys.stderr)
+        _log.exception('Unable to establish a connection with PostgreSQL. Exiting.')
         return
 
     async with Percy() as bot:
         bot.pool = pool
-        bot.alchemy_engine = create_async_engine(bot.config.alchemy_postgresql, echo=False)  # ORM
+        bot.alchemy_engine = create_async_engine(bot.config.alchemy_postgresql, echo=False)  # ORM feature
         await bot.start()
 
 
@@ -305,12 +300,12 @@ def main(ctx):
             asyncio.run(run_bot())
 
 
-@main.group(short_help='database stuff', options_metavar='[options]')
+@main.group(short_help='Database Configuration', options_metavar='[options]')
 def db():
     pass
 
 
-async def ensure_uri_can_run() -> bool:
+async def ensure_db_use() -> bool:
     connection: asyncpg.Connection = await asyncpg.connect(config.postgresql)
     await connection.close()
     return True
@@ -320,18 +315,18 @@ async def ensure_uri_can_run() -> bool:
 def init():
     """Initializes the database and creates the initial revision."""
 
-    asyncio.run(ensure_uri_can_run())
+    asyncio.run(ensure_db_use())
 
     migrations = Migrations()
     migrations.database_uri = config.postgresql
 
     try:
         applied = asyncio.run(run_upgrade(migrations))
-    except Exception:
+    except Exception:  # noqa
         traceback.print_exc()
-        click.secho('Failed to initialize and apply migrations due to error', fg='red')
+        click.secho('Failed to initialize and apply migrations. Please check your database configuration and migration scripts.', fg='red')
     else:
-        click.secho(f'Successfully initialized and applied {applied} revisions(s)', fg='green')
+        click.secho(f'Successfully initialized the database and applied {applied} migration(s).', fg='green')
 
 
 @db.command()
@@ -339,13 +334,13 @@ def init():
 def migrate(reason: str):
     """Creates a new revision for you to edit."""
     migrations = Migrations()
-    if migrations.is_next_revision_taken():
-        click.echo('An unapplied migration already exists for the next version, exiting.')
-        click.secho('Hint: Apply pending migrations with the `upgrade` command.', bold=True)
+    if migrations.is_next_revision_taken:
+        click.secho('An unapplied migration for the next version already exists. Please apply pending migrations before creating a new one.', fg='yellow')
+        click.secho('Hint: Apply pending migrations with the `upgrade` command.', fg='yellow', bold=True)
         return
 
     revision = migrations.create_revision(reason)
-    click.echo(f'Created revision V{revision.version!r}')
+    click.secho(f'Successfully created revision V{revision.version}.', fg='green')
 
 
 async def run_upgrade(migrations: Migrations) -> int:
@@ -367,24 +362,25 @@ def upgrade(sql: bool):
         applied = asyncio.run(run_upgrade(migrations))
     except Exception:
         traceback.print_exc()
-        click.secho('Failed to apply migrations due to an unexpected error.', fg='red')
+        click.secho('An error occurred while applying the database migrations. Please check your migration scripts.', fg='red')
     else:
-        click.secho(f'Successfully applied {applied} revisions(s).', fg='green')
+        click.secho(f'Successfully applied {applied} migration(s) to the database.', fg='green')
 
 
 @db.command()
 def current():
-    """Shows the current active revision version"""
+    """Shows the current active revision version."""
     migrations = Migrations()
-    click.echo(f'Version {migrations.version}')
+    as_yellow = click.style(f'{migrations.version:>03}', fg='yellow')
+    click.echo(f'Version {as_yellow}')
 
 
 @db.command()
 @click.option('--reverse', help='Print in reverse order (oldest first).', is_flag=True)
 def log(reverse: bool):
-    """Displays the revision history"""
+    """Displays the revision history."""
     migrations = Migrations()
-    # Revisions is oldest first already
+    # Revisions are oldest first already
     revs = reversed(migrations.ordered_revisions) if not reverse else migrations.ordered_revisions
     for rev in revs:
         as_yellow = click.style(f'V{rev.version:>03}', fg='yellow')
