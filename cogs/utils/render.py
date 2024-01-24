8e2769bf176bd4833ae257d68713b6a120c9fc1d
Klappstuhl <benedikt.schredl@gmx.de>
2024-01-24 21:30:00 +0100

    fixed some rendering issues with bar chart and level card

--- cogs/utils/render.py
+++ cogs/utils/render.py
@@ -98,11 +98,12 @@ class Render:
             end_index = start_index + max_keys_per_chart
             subset_data = dict(list(data.items())[start_index:end_index])
 
-            image = Image.new('RGB', (int(chart_width), int(chart_height)), color=0x1A1A1A)  # 0x020202
-            # image = Image.new('RGBA', (int(chart_width), int(chart_height)), (0, 0, 0, 0))  # Transparent
+            image = Image.new('RGB', (int(chart_width), int(chart_height)), color=0x1A1A1A)
             draw = ImageDraw.Draw(image)
 
             font = ImageFont.truetype(PATH + '/GintoBold.otf', int(LABEL_FONT_SIZE * scale_factor))
+            max_label_width = max([cls.get_text_dimensions(label, font=font)[0] for label in subset_data.keys()])
+            max_value_width = max([cls.get_text_dimensions(str(value), font=font)[0] for value in subset_data.values()])
 
             if title:
                 title_font = ImageFont.truetype(PATH + '/GintoBold.otf', int(LABEL_FONT_SIZE * scale_factor * 1.5))
@@ -110,33 +111,59 @@ class Render:
                 title_width = title_bbox[2] - title_bbox[0]
                 title_height = title_bbox[3] - title_bbox[1]
                 title_position = ((chart_width - title_width) // 2, CHART_MARGIN)
-                draw.text(title_position, title, font=title_font, fill=(255, 255, 255))  # 0xFFFFFF  # , 255
+                draw.text(
+                    title_position,
+                    title,
+                    font=title_font,
+                    fill=(255, 255, 255)
+                )
 
-                y = CHART_MARGIN + (title_height + 5) + LABEL_PADDING
+                y = CHART_MARGIN + (title_height + 5) + LABEL_PADDING * 2
             else:
                 y = CHART_MARGIN
 
             for label, value in subset_data.items():
-                bar_width = int(value / max(data.values()) * (chart_width - LABEL_PADDING * 2))
-                _origin_bar_width = bar_width
+                _, label_height = cls.get_text_dimensions(label, font=font)
+                value_width, value_height = cls.get_text_dimensions(str(value), font=font)
 
-                label_text = f'{label}: {value}'
-                label_width, label_height = font.getbbox(label_text)[2:]
-                available_width = chart_width - LABEL_PADDING - bar_width - LABEL_PADDING
-
-                if label_width > available_width:
-                    bar_width = (bar_width - label_width) - len(label_text)
-
-                draw.rounded_rectangle(((LABEL_PADDING, y), (LABEL_PADDING + bar_width, y + BAR_HEIGHT)), radius=15,
-                                       outline=BAR_COLOR, width=40, fill=BAR_COLOR)
-
-                label_position = (
-                    LABEL_PADDING + bar_width + LABEL_PADDING, y + (BAR_HEIGHT - label_height) // 2
+                # the label is aligned to the left of the image
+                label_position = (LABEL_PADDING, y + (BAR_HEIGHT - label_height) // 2)
+                draw.text(
+                    label_position,
+                    label,
+                    font=font,
+                    color=(255, 255, 255),
+                    LANCZOS=True
                 )
-                if label_width > available_width:
-                    label_position = (_origin_bar_width - label_width, y + (BAR_HEIGHT - label_height) // 2)
-                draw.text(label_position, label_text, font=font, fill=(255, 255, 255),
-                          LANCZOS=True)  # 0xFFFFFF  # , 255
+
+                bar_width = chart_width - max_label_width - max_value_width - LABEL_PADDING * 4
+                # Calculate the length of the bar by dividing the value
+                # by the max value and multiplying it by the max ar width
+                bar_width = int(value / max(data.values()) * bar_width)
+
+                # value is the count how often the command was invoked; it's displayed right on the bar
+                value_position = (LABEL_PADDING * 3 + bar_width + max_label_width, y + (BAR_HEIGHT - value_height) // 2)
+                draw.text(
+                    value_position,
+                    str(value),
+                    font=font,
+                    color=(255, 255, 255),
+                    LANCZOS=True
+                )
+
+                # bar starts after the label and has a width of max_bar_width
+                draw.rounded_rectangle(
+                    (
+                        LABEL_PADDING * 2 + max_label_width,
+                        y,
+                        LABEL_PADDING * 2 + max_label_width + bar_width,
+                        y + BAR_HEIGHT
+                    ),
+                    10,
+                    outline=BAR_COLOR,
+                    width=40,
+                    fill=BAR_COLOR
+                )
 
                 y += BAR_HEIGHT + LABEL_PADDING
 
@@ -192,7 +219,12 @@ class Render:
 
     @classmethod
     def create_outlined_rounded_rectangle(
-            cls, size: tuple[int, int], radius: int, thickness: int, fill: tuple, outline: tuple
+            cls,
+            size: tuple[int, int],
+            radius: int,
+            thickness: int,
+            fill: tuple,
+            outline: tuple
     ) -> tuple[Image.Image, Image.Image]:
         with Image.new('RGB', (size[0] + thickness, size[1] + thickness), outline) as outline_image:
             with Image.new('RGB', size, fill) as fill_image:
@@ -229,7 +261,12 @@ class Render:
         return tuple(color)
 
     @staticmethod
-    def convert_to_circle_image(image: Image, outline_colour: tuple[int, int, int], outline_width: int = 4) -> Image:
+    def convert_to_circle_image(
+            image: Image,
+            outline_colour: tuple[int, int, int],
+            outline_width: int = 4,
+            size: tuple[int, int] = (196, 196)
+    ) -> Image:
         circle_image = Image.new('RGBA', image.size, (0, 0, 0, 0))
 
         mask = Image.new('L', image.size, 0)
@@ -242,13 +279,17 @@ class Render:
         outline = Image.new('RGBA', circle_image.size, (0, 0, 0, 0))
         outline_draw = ImageDraw.Draw(outline)
         outline_draw.ellipse(
-            (outline_width, outline_width, circle_image.size[0] - outline_width,
-             circle_image.size[1] - outline_width),
+            (
+                outline_width,
+                outline_width,
+                circle_image.size[0] - outline_width,
+                circle_image.size[1] - outline_width
+            ),
             outline_colour,
             width=10
         )
         image = Image.alpha_composite(outline, circle_image)
-        return image.resize((196, 196), Image.LANCZOS)
+        return image.resize(size, Image.LANCZOS)
 
     @staticmethod
     def get_text_dimensions(text_string: str, font: ImageFont) -> tuple[int, int]:
@@ -277,54 +318,82 @@ class Render:
     ) -> BytesIO:
         with Image.open(PATH + '/template.png') as background:
             avatar = Image.open(BytesIO(avatar)).resize((196, 196), Image.BOX)
+
             mask = Image.new('L', (196, 196), 0)
-            draw = ImageDraw.Draw(mask)
-            draw.ellipse((0, 0, 196, 196), fill=255)
+            mask_draw = ImageDraw.Draw(mask)
+            mask_draw.ellipse((0, 0, 196, 196), fill=255)
 
-            avatar_rounded = Image.new('RGBA', (196, 196))
-            avatar_rounded.paste(avatar, (0, 0), mask=mask)
+            round_avatar = Image.new('RGBA', (196, 196))
+            round_avatar.paste(avatar, (0, 0), mask=mask)
 
-            background.paste(avatar_rounded, (38, 38), avatar_rounded)
+            background.paste(round_avatar, (38, 38), round_avatar)
 
-            draw = ImageDraw.Draw(background)
+            user_canvas = ImageDraw.Draw(background)
 
-            # Text for user's global name
-            draw.text((252, 62), str(user), font=GINTO_NORD_HEAVY_48, fill=(235, 235, 235))
+            user_canvas.text(
+                (252, 62),
+                str(user),
+                GINTO_NORD_HEAVY_48,
+                (235, 235, 235)
+            )
 
-            # Text for user's username
-            draw.text((252, 114), user.name, font=GINTO_BOLD_28, fill=self.get_color_alpha((216, 216, 216), 0.8))
+            user_canvas.text(
+                (252, 114),
+                user.name,
+                GINTO_BOLD_28,
+                self.get_color_alpha((216, 216, 216), 0.8)
+            )
 
-            rank_text = f'Rank #{rank}'
-            rank_text_width = draw.textlength(rank_text, font=GINTO_NORD_HEAVY_48)
-            draw.text(
-                (background.width - rank_text_width - 38, 62),
-                rank_text,
+            rank = f'Rank #{rank}'
+            rank_width = user_canvas.textlength(rank, font=GINTO_NORD_HEAVY_48)
+            user_canvas.text(
+                (
+                    background.width - rank_width - 38,
+                    62
+                ),
+                rank,
                 font=GINTO_NORD_HEAVY_48,
                 fill=(235, 235, 235),
             )
 
-            members_text = f'of {shorten_number(members)}'
-            members_text_width = draw.textlength(members_text, font=GINTO_BOLD_28)
-            draw.text(
-                (background.width - members_text_width - 38, 114),
-                members_text,
-                font=GINTO_BOLD_28,
-                fill=self.get_color_alpha((216, 216, 216), 0.8),
+            members = f'of {shorten_number(members)}'
+            members_width = user_canvas.textlength(members, font=GINTO_BOLD_28)
+            user_canvas.text(
+                (
+                    background.width - members_width - 38,
+                    114
+                ),
+                members,
+                GINTO_BOLD_28,
+                self.get_color_alpha((216, 216, 216), 0.8),
             )
 
             color = self.get_dominant_color(avatar)
             empty_bar, empty_bar_mask = self.create_outlined_rounded_rectangle(
-                (862, 38), radius=10, thickness=4, fill=self.get_color_alpha(color, 0.3), outline=color)
+                (862, 38),
+                10,
+                4,
+                self.get_color_alpha(color, 0.3),
+                color
+            )
             background.paste(empty_bar, (252, 162), empty_bar_mask)
 
             if multiplier := abs(current / required):
                 progress_bar = Image.new('RGB', (round(862 * multiplier), 38), color=color)
                 background.paste(
-                    progress_bar, (252, 164), self.create_rounded_rectangle_mask(progress_bar.size, 10))
+                    progress_bar,
+                    (252, 164),
+                    self.create_rounded_rectangle_mask(progress_bar.size, 10)
+                )
 
             level_bg, level_bg_mask = self.create_outlined_rounded_rectangle(
-                (192, 60), 20, 4, (57, 62, 70), self.get_color_alpha(color, 0.5))
-            draw = ImageDraw.Draw(level_bg)
+                (192, 60),
+                20,
+                4,
+                (57, 62, 70),
+                self.get_color_alpha(color, 0.5)
+            )
+            level_canvas = ImageDraw.Draw(level_bg)
 
             level_text = 'Level'
             level_text_width, level_text_height = self.get_text_dimensions(level_text, font=GINTO_BOLD_32)
@@ -334,15 +403,32 @@ class Render:
             text_offset_x = int((192 - (level_text_width + level_number_width + 8)) / 2)
             text_offset_y = int((60 - max(level_text_height, level_number_height)) / 2)
 
-            draw.text((text_offset_x, text_offset_y), level_text, font=GINTO_BOLD_32, fill=(216, 216, 216))
-            draw.text((text_offset_x + level_text_width + 8, text_offset_y), level_number, font=GINTO_NORD_HEAVY_36,
-                      fill=(235, 235, 235))
+            level_canvas.text(
+                (text_offset_x, text_offset_y),
+                level_text,
+                GINTO_BOLD_32,
+                (216, 216, 216)
+            )
+            level_canvas.text(
+                (
+                    text_offset_x + level_text_width + 8,
+                    text_offset_y
+                ),
+                level_number,
+                GINTO_NORD_HEAVY_36,
+                (235, 235, 235)
+            )
 
             background.paste(level_bg, (38, 254), level_bg_mask)
 
             experience_bg, experience_bg_mask = self.create_outlined_rounded_rectangle(
-                (260, 60), 20, 4, (57, 62, 70), self.get_color_alpha(color, 0.5))
-            draw = ImageDraw.Draw(experience_bg)
+                (260, 60),
+                20,
+                4,
+                (57, 62, 70),
+                self.get_color_alpha(color, 0.5)
+            )
+            experience_canvas = ImageDraw.Draw(experience_bg)
             text = f'{shorten_number(current)} XP / {shorten_number(required)}'
 
             font, y = EXPERIENCE[False]
@@ -350,14 +436,23 @@ class Render:
                 font, y = EXPERIENCE[True]
                 text_size = font.getlength(text)
 
-            offset = int((212 - text_size) / 2)
-            draw.text((offset + 28, y), text=text, font=font, fill=(235, 235, 235))
+            experience_canvas.text(
+                (int((212 - text_size) / 2) + 28, y),
+                text,
+                font,
+                (235, 235, 235)
+            )
 
             background.paste(experience_bg, (252, 254), experience_bg_mask)
 
             messages_bg, messages_bg_mask = self.create_outlined_rounded_rectangle(
-                (268, 60), 20, 4, (57, 62, 70), self.get_color_alpha(color, 0.5))
-            draw = ImageDraw.Draw(messages_bg)
+                (268, 60),
+                20,
+                4,
+                (57, 62, 70),
+                self.get_color_alpha(color, 0.5)
+            )
+            message_canvas = ImageDraw.Draw(messages_bg)
             msg_count = shorten_number(messages)
 
             count_font, text_font, count_offset, text_offset = GINTO_NORD_HEAVY_28, GINTO_BOLD_24, 16, 18
@@ -367,34 +462,47 @@ class Render:
 
             offset = int((165 - text_size) / 2)
 
-            draw.text((offset + 33, count_offset), text=msg_count, font=count_font, fill=(235, 235, 235))
-            draw.text((offset + 48 + count_font.getlength(msg_count), text_offset), text='Messages', font=text_font,
-                      fill=(216, 216, 216))
+            message_canvas.text(
+                (
+                    offset + 33,
+                    count_offset
+                ),
+                msg_count,
+                count_font,
+                (235, 235, 235)
+            )
+            message_canvas.text(
+                (
+                    offset + 48 + count_font.getlength(msg_count),
+                    text_offset
+                ),
+                'Messages',
+                text_font,
+                (216, 216, 216)
+            )
 
             background.paste(messages_bg, (846, 256), messages_bg_mask)
 
         buffer = BytesIO()
-        background.save(buffer, 'png')
+        background.save(buffer, 'PNG')
         buffer.seek(0)
         return buffer
 
     # Image Grid
 
     @staticmethod
-    def create_image_grid(images, grid_size=(2, 2)) -> BytesIO:
+    def create_image_grid(images: list[Image], grid_size=(2, 2)) -> BytesIO:
         if len(images) > grid_size[0] * grid_size[1]:
             raise ValueError("Number of images doesn't match the grid size")
 
         if len(images) < grid_size[0] * grid_size[1]:
             images += [Image.new('RGB', images[0].size, (0, 0, 0)) for _ in range(grid_size[0] * grid_size[1] - len(images))]
 
-        # Create a blank image with the appropriate size
         grid_width = max(img.width for img in images) * grid_size[0]
         grid_height = max(img.height for img in images) * grid_size[1]
 
         grid_image = Image.new('RGB', (grid_width, grid_height))
 
-        # Paste each image into the grid
         for i in range(grid_size[0]):
             for j in range(grid_size[1]):
                 index = i * grid_size[1] + j
